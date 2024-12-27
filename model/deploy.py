5443a3cc9c6cc3dd4820d9e096d8d111f0c68dd0
57112am <argha.majumder@merai.co>
2024-12-27 10:45:01 +0000

    allow unsafe werkzeug

--- model/deploy.py
+++ model/deploy.py
@@ -208,4 +208,4 @@ def handle_connect():
 
 # Starting Flask-SocketIO server
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=9900)
+    socketio.run(app, debug=True, host="0.0.0.0", port=9900, allow_unsafe_werkzeug=True)
